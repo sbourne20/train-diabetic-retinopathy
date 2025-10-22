f9f913fcf5f72e1e6d2ef23aee3addc421c8afc6
sethbourne <sbourne20@gmail.com>
2025-10-22 18:25:46 +0700

    update

--- evaluate_test_set.py
+++ evaluate_test_set.py
@@ -116,7 +116,7 @@ def evaluate_on_test_set(results_dir, dataset_path, batch_size):
         return
 
     test_transform = transforms.Compose([
-        transforms.Resize((224, 224)),
+        transforms.Resize((384, 384)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
